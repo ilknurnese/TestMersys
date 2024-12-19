9f3a142e4ba0921c9e10b8eced003eb11e4ffda5
yusufucucu <yusuffucucu@gmail.com>
2024-12-19 22:12:23 +0300

    US 6, 7, 8, 9 names have been changed

--- src/test/java/StepDefinitions/US_009_studentBalanceDetailSteps.java
+++ src/test/java/StepDefinitions/US_009_studentBalanceDetailSteps.java
@@ -2,11 +2,14 @@ package StepDefinitions;
 
 
 import Pages.MainMenuPage;
+import Utilities.GWD;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.interactions.Actions;
 
 public class US_009_studentBalanceDetailSteps {
 MainMenuPage MMP=new MainMenuPage();
+Actions actions=new Actions(GWD.getDriver());
     @And("fistly click on the student name and click on the balance details")
     public void fistlyClickOnTheStudentNameAndClickOnTheBalanceDetails() {
       MMP.myClick(MMP.studentName);
